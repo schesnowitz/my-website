480a918463d736c60fa41be45d94fdd4e47b2c40
Steve Chesnowitz <steve@chesnowitz.com>
2017-11-14 13:19:54 -0500

    style and form section

--- Gemfile
+++ Gemfile
@@ -13,6 +13,9 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
+gem 'bootstrap', '~> 4.0.0.beta2.1'
+gem 'jquery-rails'
+gem 'simple_form', '~> 3.5'
 
 
 
---
+++ links/link.rb
@@ -0,0 +1,12 @@
+# Section 6
+# bootstrap gem
+https://rubygems.org/gems/bootstrap/
+
+# bootstrap gem homepage
+https://github.com/twbs/bootstrap-rubygem
+
+# bootstrap homepage
+http://getbootstrap.com
+
+# simple form
+https://rubygems.org/gems/simple_form/versions/3.5.0
